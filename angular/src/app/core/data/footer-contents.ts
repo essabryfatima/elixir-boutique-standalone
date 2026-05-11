1182c832797b20b8f1a6315fcd844145f002b083
Fatima Es-sabry <fatima@example.com>
2026-05-11 21:44:45 +0100

    Propre: Angular 17 standalone uniquement

--- angular/src/app/core/data/footer-contents.ts
+++
@@ -1,9 +0,0 @@
-export const FOOTER_CONTENTS: Record<string, { title: string; content: string }> = {
-  faq: { title: '❓ FAQ – Questions fréquentes', content: '<p>Contenu FAQ ici</p>' },
-  livraison: { title: '🚚 Livraison & Retours', content: '<p>Contenu livraison</p>' },
-  suivi: { title: '📦 Suivi de commande', content: '<p>Contenu suivi</p>' },
-  authenticite: { title: '🔒 Garantie d’authenticité', content: '<p>Produits 100% authentiques</p>' },
-  mentions: { title: '📜 Mentions légales', content: '<p>Mentions légales...</p>' },
-  confidentialite: { title: '🔐 Confidentialité', content: '<p>Politique de confidentialité</p>' },
-  cgv: { title: '📄 Conditions Générales de Vente', content: '<p>CGV...</p>' }
-};
--- angular/src/app/shared/global-modal/global-modal.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-global-modal',
-  imports: [],
-  templateUrl: './global-modal.html',
-  styleUrl: './global-modal.scss',
-})
-export class GlobalModal {}
